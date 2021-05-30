9d43d462568533a6c130ce6f5b15d3be5736197e
Sam Patankar <50807100+Sampatankar@users.noreply.github.com>
2021-05-30 22:26:10 +0100

    Script to convert .csv to utf-8 .xlsx

---
+++ utf8_encoding.py
@@ -0,0 +1,34 @@
+import os
+import csv
+import openpyxl
+
+"""
+1. Loop through every file that is a .csv in a folder.
+2. Open each .csv
+3. Change the text encoding to utf-8
+4. Save the changes as an excel file - naming to be decided.
+"""
+
+wb = openpyxl.Workbook()
+ws = wb.active
+
+directory = r'C:\Users\Relative Sam\Desktop\dev_coding\Python\openpyxl'
+
+# Loop through every .csv file in the folder above:
+for filename in os.scandir(directory):
+    if (filename.path.endswith(".csv")) and filename.is_file():
+        print(filename.path)
+
+        # Open the .csv:
+        with open(filename.path, encoding="utf-8") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                ws.append(row)
+
+            # Remove the .csv:
+            split_string = filename.path.split(".", 1)
+            substring = split_string[0]
+            print(substring)
+
+            # Save the Excel file with .csv filename:
+            wb.save(substring + '.xlsx')
